f80b7f06e5c7218945b5371cae7681ed9893a62d
Jose Farias <jose@farias.mx>
2022-01-08 16:45:59 -0600

    Move timezone logic to Municipality

--- app/models/municipality.rb
+++ app/models/municipality.rb
@@ -2,4 +2,16 @@ class Municipality < ApplicationRecord
   belongs_to :state
 
   has_many :parishes
+
+  def timezone
+    file = File.read(TIMEZONES_FILE_PATH)
+    yaml = Psych.safe_load(file)
+
+    yaml["municipalities"][name.parameterize.underscore] ||
+      yaml["states"][state.name.parameterize.underscore]
+  end
+
+  private
+
+  TIMEZONES_FILE_PATH = Rails.root.join("config/timezones.yml")
 end
